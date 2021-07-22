f02e2f33cecbfc4816663e35f2cfc938a359c348
marieke-thomas <32941729+marieke-thomas@users.noreply.github.com>
2021-07-21 22:15:41 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+let cutekitty32 = ["Hera", "Thomas", 10, "tabby", "American shorthair", "twist ties", 9, "napping"]
+
+// print out Hera's fur color
+
+
+//print out Hera's last name
+
+
+//print out Hera's age
+
+
